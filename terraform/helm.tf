0423ef21d598e280e7c4139e0aa6e7b2c9500fa1
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2025-10-19 11:16:36 +0100

    add cert-manager helm chart

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -8,3 +8,38 @@ resource "helm_release" "nginx" {
   ]
 }
 
+
+
+## cert manager ##
+
+resource "helm_release" "cert_manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  version    = "v1.19"
+
+  create_namespace = true
+  namespace        = "cert-manager"
+
+  values = [
+    "${file("helm-values/cert-manager.yaml")}",
+  ]
+
+  depends_on = [aws_eks_pod_identity_association.cert_manager]
+}
+
+module "cert_manager_pod_identity" {
+  source = "terraform-aws-modules/eks-pod-identity/aws"
+  name   = "cert-manager"
+
+  attach_cert_manager_policy    = true
+  cert_manager_hosted_zone_arns = [local.hosted_zone_arn]
+
+}
+
+resource "aws_eks_pod_identity_association" "cert_manager" {
+  cluster_name    = module.eks.cluster_name
+  namespace       = "cert-manager"
+  service_account = "cert-manager"
+  role_arn        = module.cert_manager_pod_identity.iam_role_arn
+}
